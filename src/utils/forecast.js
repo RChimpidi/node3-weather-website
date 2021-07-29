c70dd9bfe1283097e7449565edbf188d586f8fea
rajasreechimpidi <rajasree@quadyster.com>
2021-07-29 15:36:45 -0500

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,8 @@ const forecast =(latitude, longitude, callback) => {
             callback('Unable to connect to weather service', undefined)
         } else if (body.error) { 
             callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a  ' + body.current.feelslike + '% chance  of rain')
+        } else { 
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a  ' + body.current.feelslike + '% chance  of rain. ' + 'The humidity is ' + body.current.humidity + '%')
         }
     })
 }
